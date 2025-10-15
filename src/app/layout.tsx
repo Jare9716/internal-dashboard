e5ce5d2c01457dc1c796ce054e932d776de20f13
Jare9716 <jare9716@gmail.com>
2025-10-14 22:13:55 -0500

    adding MUI and ApexCharts to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
-import "./globals.css";
+"use client";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import theme from "@/styles/theme";
+import { roboto } from "@/styles/fonts";
+import "../styles/globals.css";
 
 export default function RootLayout({
 	children,
@@ -6,8 +13,15 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`antialiased`}>{children}</body>
+		<html lang="en" className={roboto.variable}>
+			<body>
+				<AppRouterCacheProvider>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						{children}
+					</ThemeProvider>
+				</AppRouterCacheProvider>
+			</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
+
+import Button from "@mui/material/Button";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function Page() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-gray-100">
-			<Link
-				href="/dashboard"
-				className="flex items-center gap-5 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-			>
-				<span>Go to dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
+			<Link href="/dashboard">
+				{/* <span>Go to dashboard</span> <ArrowRightIcon className="w-5 md:w-6" /> */}
+				<Button
+					variant="contained"
+					size="large"
+					endIcon={<ArrowRightIcon className="w-5 md:w-6" />}
+				>
+					Go to dashboard
+				</Button>
 			</Link>
 		</div>
 	);
